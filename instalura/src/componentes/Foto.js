fa5ae434d5b7ca63d8916482adda45242b2bef2d
Tiago Luna <tiagodeluna@hotmail.com>
2018-04-25 20:41:09 -0300

    Load photos dynamically

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -18,72 +18,65 @@ class FotoAtualizacoes extends Component {
 class FotoInfo extends Component {
     render(){
         return (
-            <div className="foto-in fo">
-              <div className="foto-info-likes">
+          <div className="foto-in fo">
+            <div className="foto-info-likes">
+              {
+                this.props.foto.likers.map(liker => {
+                  return (<a href="#">{ liker.login },</a>)
+                })
+              }
+              curtiram
+            </div>
 
-                <a href="#">
-                  alots_ssa
-                </a>
+            <p className="foto-info-legenda">
+              <a className="foto-info-autor">autor </a>
+              {this.props.foto.comentario}
+            </p>
 
-                ,
-
-                <a href="#">
-                  rafael_rollo
-                </a> 
-
-                 curtiram
-             
-              </div>
-
-              <p className="foto-info-legenda">
-                <a className="foto-info-autor">autor </a>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
-              </p>
-
-              <ul className="foto-info-comentarios">
-                <li className="comentario">
-                  <a className="foto-info-autor">seguidor </a>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-                </li>
-                <li className="comentario">
-                  <a className="foto-info-autor">seguidor </a>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-                </li>
-                <li className="comentario">
-                  <a className="foto-info-autor">seguidor </a>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-                </li>
-              </ul>
-            </div>            
+            <ul className="foto-info-comentarios">
+              {
+                this.props.foto.comentarios.map(comentario => {
+                  return (
+                    <li className="comentario">
+                      <a className="foto-info-autor">{comentario.login}</a>
+                      {comentario.texto}
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </div>            
         );
     }
 }
 
+//Cabeçalho da foto
 class FotoHeader extends Component {
     render(){
         return (
             <header className="foto-header">
               <figure className="foto-usuario">
-                <img src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg" alt="foto do usuario"/>
+                <img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
                 <figcaption className="foto-usuario">
                   <a href="#">
-                    alots
+                    { this.props.foto.loginUsuario }
                   </a>  
                 </figcaption>
               </figure>
-              <time className="foto-data">03/10/2016 20:13</time>
+              <time className="foto-data">{this.props.foto.horario}</time>
             </header>            
         );
     }
 }
 
-export default class Foto extends Component {
+//Representa uma foto com cabeçalho e informações
+export default class FotoItem extends Component {
     render(){
         return (
           <div className="foto">
-            <FotoHeader/>
-            <img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2"/>
-            <FotoInfo/>
+            <FotoHeader foto={this.props.foto} />
+            <img alt="foto" className="foto-src" src={this.props.foto.urlFoto}/>
+            <FotoInfo foto={this.props.foto} />
             <FotoAtualizacoes/>
           </div>            
         );
